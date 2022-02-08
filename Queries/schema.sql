c8abc4d69b017b78bf3708ff6a7b3c2ae5ed7aea
joseperez72 <josey.perez321@gmail.com>
2022-02-08 00:59:30 -0500

    adding table schema and queries to repo

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -1,16 +1,27 @@
 -- Creating tables for PH-EmployeeDB
 CREATE TABLE departments (
-	dept_no VARCHAR(4) NOT NULL PRIMARY KEY,
-	dept_name VARCHAR(40) NOT NULL UNIQUE
+	dept_no VARCHAR(4) NOT NULL,
+	dept_name VARCHAR(40) NOT NULL UNIQUE,
+PRIMARY KEY (dept_no)
 );
 	
 CREATE TABLE employees (
-	emp_no INT NOT NULL PRIMARY KEY,
+	emp_no INT NOT NULL,
 	birth_date DATE NOT NULL,
 	first_name VARCHAR NOT NULL,
 	last_name VARCHAR NOT NULL,
 	gender VARCHAR NOT NULL,
-	hire_date DATE NOT NULL
+	hire_date DATE NOT NULL,
+PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE titles (
+	emp_no INT NOT NULL,
+	title VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no, title, from_date)
 );
 
 CREATE TABLE dept_manager (
@@ -30,4 +41,14 @@ CREATE TABLE salaries (
 	to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
 );
